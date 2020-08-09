b0714365b78fbf1bb0a0253c6685c68f778aa4ff
Paweł Kołodziej <p.kolodziej@gmail.com>
2020-08-09 17:36:34 +0300

    poprawione sumowanie.
    Signed-off-by: Paweł Kołodziej <p.kolodziej@gmail.com>

--- slowniki.py
+++ slowniki.py
@@ -2,10 +2,11 @@ def suma(*args):
     s = 0
     d = {1:3, 4:'kotek'}
     for i in args:
-        s += i
+        s += i+1
     return s
 
 def kroczaco(n):
+    """ta funkcja coś na pewno policzy"""
     k = []
     for i in range(n):
         k.append(suma(*list(range(i))))
